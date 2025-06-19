83a2567affff7680071162ceca7e938b956997c0
Ahmadk2000 <elyakhalaf@gmail.com>
2025-06-19 20:48:51 +0300

    4

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    # أضف روابط واجهات المستخدم هنا
+    path('login/', views.login_view, name='login'),
+    path('register/', views.register_view, name='register'),
 ]
--- test01/urls.py
+++ test01/urls.py
@@ -3,7 +3,11 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+# عرض الصفحة الرئيسية
+from products.views import home_view  # تأكد أن home_view موجود في products/views.py
+
 urlpatterns = [
+    path('', home_view, name='home'),  # هذا يعالج 404 عند زيارة /
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('products/', include('products.urls')),
