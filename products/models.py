c5a1d6966551f5af05ccca73badd4b5afbd77fb3
Ahmadk2000 <elyakhalaf@gmail.com>
2025-06-21 19:52:14 +0300

    9

--- products/models.py
+++ products/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from cloudinary.models import CloudinaryField  # استيراد CloudinaryField
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
@@ -13,7 +14,10 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     available = models.BooleanField(default=True)
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
-    image = models.ImageField(upload_to='products/', blank=True, null=True)  # ← تم إضافة هذا السطر
+
+    # استخدام CloudinaryField بدلاً من ImageField
+    image = CloudinaryField('image', blank=True, null=True)
+
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
